cd2d619e5f4f5c56592a5ebeac925c5ad607c332
Ch3tanUppin <sathyprakashuppin@gmail.com>
2024-11-24 17:41:13 +0530

    Minor changes

--- attacker/repeat_request.sh
+++ attacker/repeat_request.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while true; do
-    curl -H "X-Forwarded-For: 192.168.46.3" http://www.rebel-alliance.com
+    curl -H "X-Forwarded-For: 192.168.46.3" http://www.Empire.com:80
     sleep 30
 done
